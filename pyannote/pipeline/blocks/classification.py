9e590e42b531d39ba5f153eaa609905bf7e7d969
Paul Lerner <1.paul.lerner@gmail.com>
2019-12-11 10:57:15 +0100

    refactor: create a new ad-hoc PlumcotClosestAssignment class which handles multiple references targets revert the classical ClosestAssignment class to what it used to be so it doesn't break https://github.com/pyannote/pyannote-audio/blob/b7efb76e284f1cee02280dcbf2dee9b8d5c72317/pyannote/audio/pipeline/speech_turn_assignment.py#L132-L134

--- pyannote/pipeline/blocks/classification.py
+++ pyannote/pipeline/blocks/classification.py
@@ -39,6 +39,66 @@ from pyannote.core.utils.distance import l2_normalize
 
 class ClosestAssignment(Pipeline):
     """Assign each sample to the closest target
+    Parameters
+    ----------
+    metric : `str`, optional
+        Distance metric. Defaults to 'cosine'
+    normalize : `bool`, optional
+        L2 normalize vectors before clustering.
+    Hyper-parameters
+    ----------------
+    threshold : `float`
+        Do not assign if distance greater than `threshold`.
+    """
+
+    def __init__(self, metric: Optional[str] = 'cosine',
+                       normalize: Optional[bool] = False):
+
+        super().__init__()
+        self.metric = metric
+        self.normalize = normalize
+
+        min_dist, max_dist = dist_range(metric=self.metric,
+                                        normalize=self.normalize)
+        if not np.isfinite(max_dist):
+            # this is arbitray and might lead to suboptimal results
+            max_dist = 1e6
+            msg = (f'bounding distance threshold to {max_dist:g}: '
+                   f'this might lead to suboptimal results.')
+            warnings.warn(msg)
+        self.threshold = Uniform(min_dist, max_dist)
+
+    def __call__(self, X_target, X):
+        """Assign each sample to its closest class (if close enough)
+        Parameters
+        ----------
+        X_target : `np.ndarray`
+            (n_targets, n_dimensions) target embeddings
+        X : `np.ndarray`
+            (n_samples, n_dimensions) sample embeddings
+        Returns
+        -------
+        assignments : `np.ndarray`
+            (n_samples, ) sample assignments
+        """
+
+        if self.normalize:
+            X_target = l2_normalize(X_target)
+            X = l2_normalize(X)
+
+        distance = cdist(X_target, X, metric=self.metric)
+        targets = np.argmin(distance, axis=0)
+
+        for i, k in enumerate(targets):
+            if distance[k, i] > self.threshold:
+                # do not assign
+                targets[i] = -i
+
+        return targets
+
+class PlumcotClosestAssignment(Pipeline):
+    """Assign each sample to the closest target
+    In plumcot there targets have multiple references.
 
     Parameters
     ----------
@@ -76,15 +136,12 @@ class ClosestAssignment(Pipeline):
         Parameters
         ----------
         X_target :
-            Either : `np.ndarray`
-                (n_targets, n_dimensions) target embeddings
-            Or : iterable of n_targets
+            iterable of n_targets:
                 all targets are `np.ndarray` which shape can differ:
                 (m_samples, n_dimensions) target embeddings
         X : `np.ndarray`
             (n_samples, n_dimensions) sample embeddings
         method : `str`, optional
-            Relevant iff the targets have several samples
             Used to reduce the distance matrix to a single float
             Available options (case-insensitive):
                 - 'mean' (Default)
@@ -105,21 +162,18 @@ class ClosestAssignment(Pipeline):
                     X_target[i]= l2_normalize(target)
             X = l2_normalize(X)
 
-        if isinstance(X_target,np.ndarray):
-            distances = cdist(X_target, X, metric=self.metric)
-        else:
-            distances=[]
-            for target in X_target:
-                distance = cdist(target, X, metric=self.metric)
-                if method=='mean':
-                    distance=np.mean(distance)
-                elif method=='median':
-                    distance=np.median(distance)
-                elif method=='min':
-                    distance=np.min(distance)
-                else:
-                    raise ValueError(f"{method} is an invalid value for method, see \n{help(classifier)}")
-                distances.append(distance)
+        distances=[]
+        for target in X_target:
+            distance = cdist(target, X, metric=self.metric)
+            if method=='mean':
+                distance=np.mean(distance)
+            elif method=='median':
+                distance=np.median(distance)
+            elif method=='min':
+                distance=np.min(distance)
+            else:
+                raise ValueError(f"{method} is an invalid value for method, see \n{help(self)}")
+            distances.append(distance)
         target = np.argmin(distances, axis=0)
 
         if distances[target] > self.threshold:
